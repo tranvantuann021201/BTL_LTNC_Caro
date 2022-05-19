cb0d75e6fcdb1f6c75b1d5e3ab8bcc1fe79a1af4
tranvantuann021201 <90585817+tranvantuann021201@users.noreply.github.com>
2022-05-19 12:06:59 +0700

    design

---
+++ game_Co_Caro/game_Co_Caro/Form1.Designer.cs
@@ -0,0 +1,380 @@
+﻿namespace game_Co_Caro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tùyChọnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chơiMớiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tínhLạiTỉSốToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thoátToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.tinhDiemO = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.tinhDiemX = new System.Windows.Forms.Label();
+            this.btn_0_0 = new System.Windows.Forms.Button();
+            this.btn_0_1 = new System.Windows.Forms.Button();
+            this.btn_0_2 = new System.Windows.Forms.Button();
+            this.btn_1_0 = new System.Windows.Forms.Button();
+            this.btn_1_1 = new System.Windows.Forms.Button();
+            this.btn_1_2 = new System.Windows.Forms.Button();
+            this.btn_2_0 = new System.Windows.Forms.Button();
+            this.btn_2_1 = new System.Windows.Forms.Button();
+            this.btn_2_2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.btn_2_2, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn_2_1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn_2_0, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn_1_2, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn_1_1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn_1_0, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn_0_2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn_0_1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn_0_0, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 96);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(364, 343);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tùyChọnToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(364, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tùyChọnToolStripMenuItem
+            // 
+            this.tùyChọnToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.chơiMớiToolStripMenuItem,
+            this.tínhLạiTỉSốToolStripMenuItem,
+            this.thoátToolStripMenuItem});
+            this.tùyChọnToolStripMenuItem.Name = "tùyChọnToolStripMenuItem";
+            this.tùyChọnToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.tùyChọnToolStripMenuItem.Text = "Tùy chọn";
+            // 
+            // chơiMớiToolStripMenuItem
+            // 
+            this.chơiMớiToolStripMenuItem.Name = "chơiMớiToolStripMenuItem";
+            this.chơiMớiToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.chơiMớiToolStripMenuItem.Text = "Chơi mới";
+            // 
+            // tínhLạiTỉSốToolStripMenuItem
+            // 
+            this.tínhLạiTỉSốToolStripMenuItem.Name = "tínhLạiTỉSốToolStripMenuItem";
+            this.tínhLạiTỉSốToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.tínhLạiTỉSốToolStripMenuItem.Text = "Tính lại tỉ số";
+            // 
+            // thoátToolStripMenuItem
+            // 
+            this.thoátToolStripMenuItem.Name = "thoátToolStripMenuItem";
+            this.thoátToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.thoátToolStripMenuItem.Text = "Thoát";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 41.20879F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 58.79121F));
+            this.tableLayoutPanel2.Controls.Add(this.panel4, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.panel2, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.panel3, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(364, 73);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.Controls.Add(this.tinhDiemO);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(152, 39);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(209, 31);
+            this.panel4.TabIndex = 3;
+            // 
+            // tinhDiemO
+            // 
+            this.tinhDiemO.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.tinhDiemO.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tinhDiemO.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tinhDiemO.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tinhDiemO.Location = new System.Drawing.Point(0, 0);
+            this.tinhDiemO.Name = "tinhDiemO";
+            this.tinhDiemO.Size = new System.Drawing.Size(209, 31);
+            this.tinhDiemO.TabIndex = 4;
+            this.tinhDiemO.Text = "0";
+            this.tinhDiemO.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 39);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(143, 31);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "   Người chơi O:";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(143, 30);
+            this.panel2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "   Người chơi X:";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this.tinhDiemX);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(152, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(209, 30);
+            this.panel3.TabIndex = 2;
+            // 
+            // tinhDiemX
+            // 
+            this.tinhDiemX.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.tinhDiemX.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tinhDiemX.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tinhDiemX.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tinhDiemX.Location = new System.Drawing.Point(0, 0);
+            this.tinhDiemX.Name = "tinhDiemX";
+            this.tinhDiemX.Size = new System.Drawing.Size(209, 30);
+            this.tinhDiemX.TabIndex = 3;
+            this.tinhDiemX.Text = "0";
+            this.tinhDiemX.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_0_0
+            // 
+            this.btn_0_0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_0_0.Location = new System.Drawing.Point(3, 3);
+            this.btn_0_0.Name = "btn_0_0";
+            this.btn_0_0.Size = new System.Drawing.Size(115, 108);
+            this.btn_0_0.TabIndex = 0;
+            this.btn_0_0.Text = " ";
+            this.btn_0_0.UseVisualStyleBackColor = true;
+            // 
+            // btn_0_1
+            // 
+            this.btn_0_1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_0_1.Location = new System.Drawing.Point(124, 3);
+            this.btn_0_1.Name = "btn_0_1";
+            this.btn_0_1.Size = new System.Drawing.Size(115, 108);
+            this.btn_0_1.TabIndex = 1;
+            this.btn_0_1.Text = " ";
+            this.btn_0_1.UseVisualStyleBackColor = true;
+            // 
+            // btn_0_2
+            // 
+            this.btn_0_2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_0_2.Location = new System.Drawing.Point(245, 3);
+            this.btn_0_2.Name = "btn_0_2";
+            this.btn_0_2.Size = new System.Drawing.Size(116, 108);
+            this.btn_0_2.TabIndex = 2;
+            this.btn_0_2.Text = " ";
+            this.btn_0_2.UseVisualStyleBackColor = true;
+            // 
+            // btn_1_0
+            // 
+            this.btn_1_0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_1_0.Location = new System.Drawing.Point(3, 117);
+            this.btn_1_0.Name = "btn_1_0";
+            this.btn_1_0.Size = new System.Drawing.Size(115, 108);
+            this.btn_1_0.TabIndex = 3;
+            this.btn_1_0.Text = " ";
+            this.btn_1_0.UseVisualStyleBackColor = true;
+            // 
+            // btn_1_1
+            // 
+            this.btn_1_1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_1_1.Location = new System.Drawing.Point(124, 117);
+            this.btn_1_1.Name = "btn_1_1";
+            this.btn_1_1.Size = new System.Drawing.Size(115, 108);
+            this.btn_1_1.TabIndex = 4;
+            this.btn_1_1.Text = " ";
+            this.btn_1_1.UseVisualStyleBackColor = true;
+            // 
+            // btn_1_2
+            // 
+            this.btn_1_2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_1_2.Location = new System.Drawing.Point(245, 117);
+            this.btn_1_2.Name = "btn_1_2";
+            this.btn_1_2.Size = new System.Drawing.Size(116, 108);
+            this.btn_1_2.TabIndex = 5;
+            this.btn_1_2.Text = " ";
+            this.btn_1_2.UseVisualStyleBackColor = true;
+            // 
+            // btn_2_0
+            // 
+            this.btn_2_0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_2_0.Location = new System.Drawing.Point(3, 231);
+            this.btn_2_0.Name = "btn_2_0";
+            this.btn_2_0.Size = new System.Drawing.Size(115, 109);
+            this.btn_2_0.TabIndex = 6;
+            this.btn_2_0.Text = " ";
+            this.btn_2_0.UseVisualStyleBackColor = true;
+            // 
+            // btn_2_1
+            // 
+            this.btn_2_1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_2_1.Location = new System.Drawing.Point(124, 231);
+            this.btn_2_1.Name = "btn_2_1";
+            this.btn_2_1.Size = new System.Drawing.Size(115, 109);
+            this.btn_2_1.TabIndex = 7;
+            this.btn_2_1.Text = " ";
+            this.btn_2_1.UseVisualStyleBackColor = true;
+            // 
+            // btn_2_2
+            // 
+            this.btn_2_2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_2_2.Location = new System.Drawing.Point(245, 231);
+            this.btn_2_2.Name = "btn_2_2";
+            this.btn_2_2.Size = new System.Drawing.Size(116, 109);
+            this.btn_2_2.TabIndex = 8;
+            this.btn_2_2.Text = " ";
+            this.btn_2_2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleGreen;
+            this.ClientSize = new System.Drawing.Size(364, 439);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cờ Caro 3x3";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btn_2_2;
+        private System.Windows.Forms.Button btn_2_1;
+        private System.Windows.Forms.Button btn_2_0;
+        private System.Windows.Forms.Button btn_1_2;
+        private System.Windows.Forms.Button btn_1_1;
+        private System.Windows.Forms.Button btn_1_0;
+        private System.Windows.Forms.Button btn_0_2;
+        private System.Windows.Forms.Button btn_0_1;
+        private System.Windows.Forms.Button btn_0_0;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tùyChọnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem chơiMớiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tínhLạiTỉSốToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thoátToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label tinhDiemO;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label tinhDiemX;
+    }
+}
+
---
+++ game_Co_Caro/game_Co_Caro/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace game_Co_Caro
+{
+    public partial class Form1 : Form
+    {
+        Boolean check;
+        int demTiSo;
+
+        private void enable_false()
+        {
+            btn_0_0.Enabled = false;
+            btn_0_1.Enabled = false;
+            btn_0_2.Enabled = false;
+            btn_1_0.Enabled = false;
+            btn_1_1.Enabled = false;
+            btn_1_2.Enabled = false;
+            btn_2_0.Enabled = false;
+            btn_2_1.Enabled = false;
+            btn_2_2.Enabled = false;
+        }
+
+        private void tinhDiem()
+        {
+            if (btn_0_0.Text == "X" && btn_0_1.Text == "X" && btn_0_2.Text == "X")
+            {
+                btn_0_0.BackColor = Color.Aqua;
+                btn_0_1.BackColor = Color.Aqua;
+                btn_0_2.BackColor = Color.Aqua;
+                
+                MessageBox.Show("Người chơi X giành chiến thắng", "Cờ Caro 3x3", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                demTiSo = int.Parse(tinhDiemX.Text);
+                tinhDiemX.Text = Convert.ToString(demTiSo);
+                enable_false();
+            }
+
+            if (btn_1_0.Text == "X" && btn_1_1.Text == "X" && btn_1_2.Text == "X")
+            {
+                btn_0_0.BackColor = Color.Aqua;
+                btn_0_1.BackColor = Color.Aqua;
+                btn_0_2.BackColor = Color.Aqua;
+
+                MessageBox.Show("Người chơi X giành chiến thắng", "Cờ Caro 3x3", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                demTiSo = int.Parse(tinhDiemX.Text);
+                tinhDiemX.Text = Convert.ToString(demTiSo);
+                enable_false();
+            }
+
+            if (btn_2_0.Text == "X" && btn_2_1.Text == "X" && btn_2_2.Text == "X")
+            {
+                btn_0_0.BackColor = Color.Aqua;
+                btn_0_1.BackColor = Color.Aqua;
+                btn_0_2.BackColor = Color.Aqua;
+
+                MessageBox.Show("Người chơi X giành chiến thắng", "Cờ Caro 3x3", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                demTiSo = int.Parse(tinhDiemX.Text);
+                tinhDiemX.Text = Convert.ToString(demTiSo);
+                enable_false();
+            }
+
+            if (btn_0_0.Text == "X" && btn_1_0.Text == "X" && btn_2_0.Text == "X")
+            {
+                btn_0_0.BackColor = Color.Aqua;
+                btn_0_1.BackColor = Color.Aqua;
+                btn_0_2.BackColor = Color.Aqua;
+
+                MessageBox.Show("Người chơi X giành chiến thắng", "Cờ Caro 3x3", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                demTiSo = int.Parse(tinhDiemX.Text);
+                tinhDiemX.Text = Convert.ToString(demTiSo);
+                enable_false();
+            }
+
+            if (btn_0_0.Text == "X" && btn_0_1.Text == "X" && btn_0_2.Text == "X")
+            {
+                btn_0_0.BackColor = Color.Aqua;
+                btn_0_1.BackColor = Color.Aqua;
+                btn_0_2.BackColor = Color.Aqua;
+
+                MessageBox.Show("Người chơi X giành chiến thắng", "Cờ Caro 3x3", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                demTiSo = int.Parse(tinhDiemX.Text);
+                tinhDiemX.Text = Convert.ToString(demTiSo);
+                enable_false();
+            }
+
+            if (btn_0_0.Text == "X" && btn_0_1.Text == "X" && btn_0_2.Text == "X")
+            {
+                btn_0_0.BackColor = Color.Aqua;
+                btn_0_1.BackColor = Color.Aqua;
+                btn_0_2.BackColor = Color.Aqua;
+
+                MessageBox.Show("Người chơi X giành chiến thắng", "Cờ Caro 3x3", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                demTiSo = int.Parse(tinhDiemX.Text);
+                tinhDiemX.Text = Convert.ToString(demTiSo);
+                enable_false();
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
